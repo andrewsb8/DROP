8f9e6e917f23c3bd99c85bdf33fca5655cc7693a
bandrews <b9andrews@gmail.com>
2024-11-29 12:28:32 -0500

    update README

--- src/dropinfo/dropinfo.c
+++ src/dropinfo/dropinfo.c
@@ -5,6 +5,6 @@ const char *usage_messg =
 	"USAGE: drop command [OPTIONS]. Use drop -? or drop --help for more information.\n";
 const char *program_bug_address =
 	"https://github.com/andrewsb8/DROP/issues";
-const char *program_version = "Version 2024.1";
+const char *program_version = "Version 2025.1-dev";
 const char *program_desc =
 	"-- A command line tool to investigate protein structures via direct manipulation of dihedral angles. --";
